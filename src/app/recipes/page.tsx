cdb540bfc42b280ef4702daded6d16de3cc9f53b
JohnGrubba <nicjontrickshots@gmail.com>
2025-03-17 09:16:02 +0100

    Remove unused query

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,14 +1,7 @@
 import RecipeList from '@/components/RecipeList'
-import { prisma } from '@/lib/prismaclient'
 import React from 'react'
 
 const page = async () => {
-    const recipes = await prisma.rezept.findMany({
-        include: {
-            steps: true,
-            zutaten: true
-        }
-    })
     return (
         <RecipeList type="all" />
     )
